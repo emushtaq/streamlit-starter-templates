a3803330e5b6f18f17d404d313d01155116b0a85
emushtaq <J5zH7!s3>
2020-10-28 12:00:37 +0100

    updated header

--- streamlit_spacy.py
+++ streamlit_spacy.py
@@ -1,5 +1,10 @@
+import streamlit as st
 import spacy_streamlit
 
 models = ["en_core_web_sm", "en_core_web_md"]
 default_text = "Sundar Pichai is the CEO of Google."
+
+st.header("Demo - Spacy token visualization")
+st.text("This is a prototype to try out Spacy and Streamlit")
+
 spacy_streamlit.visualize(models, default_text)
